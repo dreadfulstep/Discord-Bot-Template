4072898de15eb00f51541a3a4b16c18f711a0ea4
dreadfulstep <110997762+dreadfulstep@users.noreply.github.com>
2023-10-08 03:12:07 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection, ActivityType, Events, AuditLogEvent } = require(`discord.js`);
+const fs = require('fs');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] }); 
+
+client.on("ready", async (client) => {
+    client.user.setPresence({ activities: [{ name: `/help`, type: ActivityType.Playing }]});
+}) 
+
+client.commands = new Collection()
+
+
+require('dotenv').config();
+
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands");
+
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.token)
+})();
